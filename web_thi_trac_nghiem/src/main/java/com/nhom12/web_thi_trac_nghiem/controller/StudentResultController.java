cb18b5f431c5e3b0981ee58db2c1b342b251a81f
liwi6 <156945289+nganhthu@users.noreply.github.com>
2024-04-09 10:45:31 +0700

    Create StudentResultController.java

---
+++ web_thi_trac_nghiem/src/main/java/com/nhom12/web_thi_trac_nghiem/controller/StudentResultController.java
@@ -0,0 +1,16 @@
+package com.nhom12.web_thi_trac_nghiem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class StudentResultController {
+
+    // Trang xem kết quả của từng sinh viên
+    @GetMapping("/student/results")
+    public String showStudentResultsPage() {
+        return "student_results"; // Trả về view của trang xem kết quả của sinh viên
+    }
+}
