7588d420064de99711eecd95c5290a3faa5941b6
liwi6 <156945289+nganhthu@users.noreply.github.com>
2024-04-09 10:46:14 +0700

    Create WebThiTracNghiemApplication.java

---
+++ web_thi_trac_nghiem/src/main/java/com/nhom12/web_thi_trac_nghiem/WebThiTracNghiemApplication.java
@@ -0,0 +1,13 @@
+package com.nhom12.web_thi_trac_nghiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThiTracNghiemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebThiTracNghiemApplication.class, args);
+    }
+
+}
