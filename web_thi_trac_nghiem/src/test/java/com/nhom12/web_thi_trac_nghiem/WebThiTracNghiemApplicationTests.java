98dd8eec8f417fb2d47b04196c0d1bc53032117c
liwi6 <156945289+nganhthu@users.noreply.github.com>
2024-04-09 10:46:25 +0700

    Create WebThiTracNghiemApplicationTests.java

---
+++ web_thi_trac_nghiem/src/test/java/com/nhom12/web_thi_trac_nghiem/WebThiTracNghiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom12.web_thi_trac_nghiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebThiTracNghiemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
